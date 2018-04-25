8e3d6ed7c0a2d8882bc021aa0e4cdc6cc264a17a
Sam <samfoong88@gmail.com>
2018-04-25 23:58:51 +1000

    add association between user and articles and add debugger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :articles
+    before_save { self.email = email.downcase }
     validates :username, presence: true, 
                 uniqueness:{ case_sensitive: false},
                 length: { minimum: 3, maximum: 25 }
@@ -7,5 +9,4 @@ class User < ApplicationRecord
                 length: { maximum: 105},
                 uniqueness: {case_sensitive: false },
                 format: {with: VALID_EMAIL_REGEX }
-
 end
